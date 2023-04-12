39b638dbe406eaac174619cc0050b78eaa9cfc7f
Fabrizio1919 <fabriziosalerno19@gmail.com>
2023-04-12 15:08:08 +0200

    Style in page

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -10,10 +10,41 @@ export default {
 </script>
 
 <template>
-  <h1>Hello World</h1>
+  <div class="container">
+    <h1>Hello World</h1>
+  </div>
   
 </template>
 
 <style>
+h1 {
+  color:brown;
+  padding: 115px 0;
+}
+
+.container {
+	--angle: 0deg;
+	width: 70vmin;
+	height: 50vmin;
+	border: 4vmin solid;
+	/* This will work but yield a wrong result: */
+	border-image: linear-gradient(var(--angle), red, yellow, lime, aqua, blue, magenta) 1;
+	animation: 10s rotate linear infinite;
+  box-shadow: 0px 15px 20px rgba(0, 0, 0, 0.2);
+}
+
+@property --angle {
+	syntax: '<angle>';
+	initial-value: 0deg;
+	inherits: false;
+}
+
+@keyframes rotate {
+	to {
+		--angle: 360deg;
+	}
+}
+
+
 
 </style>
